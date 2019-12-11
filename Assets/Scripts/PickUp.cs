44c673b03fa951994fe8819caba6b1d88691065d
FlashFusionGitHub <rowen.govender93@gmail.com>
2019-12-11 22:26:25 +1030

    Added animations to player character
    + Added a jet pack to the player.

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    enum Size { small = 1, medium, large };
+
+    [SerializeField]
+    Size size { get; set; }
+    [SerializeField]
+    int worth { get; set; }
+    [SerializeField]
+    float fuel { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,25 +11,61 @@ public class PlayerController : MonoBehaviour
 {
     // Players max horizontal movement speed
     public int moveSpeed;
+    // Players vertical propulsion force - for jetpack
+    public int propulsionForce;
     // Players max vertical jumping force
     public int jumpForce;
     // The players current grounded state - is the player touching a platform
     public bool isGrounded;
     // Reference to the players rigidbody component
     Rigidbody2D rb;
+    //Reference to the players animator
+    Animator anim;
+    // Jetpack current fuel amount
+    public float fuel;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get The rigidbody component
+        // Get the players rigidbody component
         rb = GetComponent<Rigidbody2D>();
+        // Get the players animator component
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveHorizontal();
-        Jump();
+        Propulsion();
+
+        if(isGrounded)
+            Jump();
+
+        if (fuel > 0)
+        {
+            // check player is touching ground - set isJumping animator state accordingly
+            if (isGrounded)
+            {
+                anim.SetBool("isJumping", false);
+            }
+            else if (!isGrounded)
+            {
+                anim.SetBool("isJumping", true);
+            }
+        }
+        else
+        {
+            // check player is touching ground - set isJumping animator state accordingly
+            if (isGrounded)
+            {
+                anim.SetBool("isFalling", false);
+            }
+            else if (!isGrounded)
+            {
+                anim.SetBool("isFalling", true);
+            }
+        }
     }
 
     void MoveHorizontal()
@@ -40,18 +76,34 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
         // Rotate player transform to face its move direction
-        if (moveInput < 0)
+        if (moveInput < 0) {
             transform.rotation = new Quaternion(0, 180, 0, 0);
-        if (moveInput > 0)
-            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
 
+        if (moveInput > 0) {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        // set animator to walking if horizontal axis input is applied
+        anim.SetFloat("walking", Mathf.Abs(moveInput));
+    }
+
+    void Propulsion()
+    {
+        // Apply an Upward force to the player while the UpArrow is pressed and the player is touching a platform
+        if (Input.GetKey(KeyCode.UpArrow) && fuel > 0)
+        {
+            rb.AddForce(new Vector2(0, propulsionForce), ForceMode2D.Force);
+        }
     }
 
     void Jump()
     {
         // Apply an Upward force to the player if the UpArrow is pressed and the player is touching a platform
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+        }
     }
 
     // Check when the player enters or exits and platform - update grounded state accordingly
